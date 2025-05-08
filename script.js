1082dbba1c9a5d3e5937abd02216f4cb01d21ca7
Pedro Soares <ps808684@gmail.com>
2025-05-08 09:54:21 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,52 @@
+console.log("hello world!");
+console.log(45);
+console.log("hoje eu tenho 20 anos");
+console.log(293092000220272);
+console.log(true);
+console.log(undefined);
+console.log(null);
+console.log(23.33);
+console.log(false);
+
+// tipos de dados primitivos
+
+// texto- strings (aspas duplas, aspas simples e crase)
+// string com aspas duplas
+"isto é um texto , porque está dentro de aspas.";
+// string com aspas simples
+'isso também é um texto pois está em aspas simples.'
+ // string com crase
+ 
+
+// tipo números- numbers
+// int (obs:não existe int ou float no js)
+10;
+234;
+// float (obs:não existe int ou float no js)
+1.45;
+0.76;
+
+// tipo boolean
+true;
+false;
+
+// tipo undefined
+undefined;
+
+// tipo null
+null;
+
+// criando variáveis
+// duas maneiras (let e const)
+// boas práticas para variáveis -> escolha bons nomes, descritivos. se tiver apenas uma palavra que seja tudo minúsculo, pórem caso tenha mais de uma palavraa, o restante inicia com letra maiúscula (camel case)
+
+let primeiroNome = "Pedro";
+let segundoNome = "Soares";
+
+const minhaIdade = 20;
+console.log(primeiroNome)
+console.log(minhaIdade)
+primeiroNome= "Paulo",
+console.log(primeiroNome)
+let endereço= "Av. Leite Barbosa, 941 - Altos"
+console.log(endereço)
